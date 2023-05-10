d8a599b5cea3abde4920d257e68e1e170cf13ea9
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-10 20:15:45 -0300

    Feat: post new games controller

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -2,8 +2,22 @@ import { db } from "../database/database.connection.js";
 
 export async function getGames(req, res) {
   try {
-    const {rows: games} = await db.query(`SELECT * FROM games`);
+    const { rows: games } = await db.query(`SELECT * FROM games;`);
     res.send(games);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function addGame(req, res) {
+  const { name, image, stockTotal, pricePerDay } = req.body;
+  console.log(req.body);
+  try {
+    await db.query(
+      `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,
+      [name, image, stockTotal, pricePerDay]
+    );
+    res.sendStatus(201);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- src/routers/games.router.js
+++ src/routers/games.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getGames } from "../controllers/games.controller.js";
+import { addGame, getGames } from "../controllers/games.controller.js";
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
+gamesRouter.post("/games", addGame);
 
 export default gamesRouter;
