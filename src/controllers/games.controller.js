af38e054481a29804cb77fef2b17db614798e069
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-10 19:59:52 -0300

    Feat: get games list controller

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,5 +1,10 @@
 import { db } from "../database/database.connection.js";
 
-export async function getGames(req, res){
-    res.send("placeholder, games");
+export async function getGames(req, res) {
+  try {
+    const {rows: games} = await db.query(`SELECT * FROM games`);
+    res.send(games);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
