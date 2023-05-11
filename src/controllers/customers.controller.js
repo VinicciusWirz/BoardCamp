3d652f0066b97fa14eb3a380635d11fec84ec917
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-10 22:22:06 -0300

    Feat: post new customer into db

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -14,3 +14,17 @@ export async function getCustomers(req, res) {
     res.status(500).send(error.message);
   }
 }
+
+export async function addCustomer(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+
+  try {
+    await db.query(
+      `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)`,
+      [name, phone, cpf, birthday]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routers/customers.router.js
+++ src/routers/customers.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getCustomers } from "../controllers/customers.controller.js";
+import { addCustomer, getCustomers } from "../controllers/customers.controller.js";
 
 const customersRouter = Router();
 
 customersRouter.get("/customers/:id", getCustomers);
+customersRouter.post("/customers", addCustomer);
 
 export default customersRouter;
