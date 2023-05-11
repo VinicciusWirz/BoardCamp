8fac5e875d109832031d7571e30bed0ab0fd23c8
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-10 22:42:20 -0300

    Feat: post new customers validations

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -19,6 +19,11 @@ export async function addCustomer(req, res) {
   const { name, phone, cpf, birthday } = req.body;
 
   try {
+    const customer = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [
+      cpf,
+    ]);
+    if (customer) return res.sendStatus(409);
+    
     await db.query(
       `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)`,
       [name, phone, cpf, birthday]
--- src/routers/customers.router.js
+++ src/routers/customers.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addCustomer, getCustomers } from "../controllers/customers.controller.js";
+import schemaValidation from "../middlewares/schemaValidation.middleware.js";
+import { customerSchema } from "../schemas/customer.schema.js";
 
 const customersRouter = Router();
 
 customersRouter.get("/customers/:id", getCustomers);
-customersRouter.post("/customers", addCustomer);
+customersRouter.post("/customers", schemaValidation(customerSchema), addCustomer);
 
 export default customersRouter;
