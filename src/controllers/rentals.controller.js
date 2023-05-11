28fbcf286431b38617a0d07baab5cce74e7e3b0e
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-11 06:30:14 -0300

    Feat: get rentals from database

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,7 +1,39 @@
 import { db } from "../database/database.connection.js";
 
-export async function getRentals(req, res){
-    res.send("placeholder, rentals");
+export async function getRentals(req, res) {
+  try {
+    const query = `
+        SELECT
+            rentals.*,
+            customers.id AS customer_id,
+            customers.name AS customer_name,
+            games.id AS game_id,
+            games.name AS game_name
+        FROM rentals
+        JOIN customers ON rentals."customerId" = customers.id
+        JOIN games ON rentals."gameId" = games.id
+        ;`;
+
+    const { rows, rowCount } = await db.query(query);
+    if (!rowCount) return res.send([]);
+
+    const rentalsList = rows.map((r) => ({
+      ...r,
+      rentDate: r.rentDate.toLocaleDateString("en-CA"),
+      customer: { id: r.customer_id, name: r.customer_name },
+      game: { id: r.game_id, name: r.game_name },
+    }));
+    rentalsList.forEach((r) => {
+      delete r.game_id;
+      delete r.game_name;
+      delete r.customer_id;
+      delete r.customer_name;
+    });
+
+    res.send(rentalsList);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
 export async function addRental(req, res) {
