6ed34a5f5571e4f06542970f68f51bb8f8864326
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-11 06:06:41 -0300

    Feat: post new rental schema validation

--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addRental, getRentals } from "../controllers/rentals.controller.js";
+import schemaValidation from "../middlewares/schemaValidation.middleware.js";
+import { rentalSchema } from "../schemas/rental.schema.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
-rentalsRouter.post("/rentals", addRental);
+rentalsRouter.post("/rentals", schemaValidation(rentalSchema), addRental);
 
 export default rentalsRouter;
