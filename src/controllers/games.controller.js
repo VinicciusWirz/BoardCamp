ebbbbf185a25c6e35bb67600a6435092a70a381c
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-10 21:38:02 -0300

    Feat: post new games validations

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -11,8 +11,10 @@ export async function getGames(req, res) {
 
 export async function addGame(req, res) {
   const { name, image, stockTotal, pricePerDay } = req.body;
-  console.log(req.body);
   try {
+    const game = await db.query(`SELECT * FROM games WHERE name=$1;`, [name]);
+    if (game.rowCount)
+      return res.status(409).send("Game is already in db");
     await db.query(
       `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,
       [name, image, stockTotal, pricePerDay]
--- src/routers/games.router.js
+++ src/routers/games.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { addGame, getGames } from "../controllers/games.controller.js";
+import { gameSchema } from "../schemas/games.schema.js";
+import schemaValidation from "../middlewares/schemaValidation.middleware.js";
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
-gamesRouter.post("/games", addGame);
+gamesRouter.post("/games", schemaValidation(gameSchema), addGame);
 
 export default gamesRouter;
