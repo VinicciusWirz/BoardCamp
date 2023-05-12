737f01857d9dd19f9ceb78cdf127d95a5bb992f4
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-11 21:17:08 -0300

    Feat: games search query

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,8 +1,11 @@
 import { db } from "../database/database.connection.js";
 
 export async function getGames(req, res) {
+  const name = req.query.name;
   try {
-    const { rows: games } = await db.query(`SELECT * FROM games;`);
+    const { rows: games } = name
+      ? await db.query(`SELECT * FROM games WHERE LOWER(name) LIKE LOWER($1);`, [`${name}%`])
+      : await db.query(`SELECT * FROM games;`);
     res.send(games);
   } catch (error) {
     res.status(500).send(error.message);
@@ -13,8 +16,7 @@ export async function addGame(req, res) {
   const { name, image, stockTotal, pricePerDay } = req.body;
   try {
     const game = await db.query(`SELECT * FROM games WHERE name=$1;`, [name]);
-    if (game.rowCount)
-      return res.status(409).send("Game is already in db");
+    if (game.rowCount) return res.status(409).send("Game is already in db");
     await db.query(
       `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,
       [name, image, stockTotal, pricePerDay]
